d28c7148262f32c7b4980d7d921491b6bd1bf479
red <red@mail.com>
2024-05-24 00:44:33 +0300

    r13

---
+++ commit13/F.java
@@ -0,0 +1,58 @@
+public class F extends null implements D {
+
+    private byte d = 1;
+
+    private String h = "test";
+
+    public Object gg() {
+        return new java.util.Random();
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-6);
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int hh() {
+        return new java.util.Random().nextInt();
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public void aa() {
+        System.out.println("void aa");
+    }
+
+    public long ac() {
+        return 333;
+    }
+
+    public float ff() {
+        return 3.14;
+    }
+
+    public long dd() {
+        return 33;
+    }
+
+    public int af() {
+        return -1;
+    }
+
+    public double ee() {
+        return 0.000001;
+    }
+
+    public Object rr() {
+        return null;
+    }
+}
---
+++ commit13/J.java
@@ -0,0 +1,50 @@
+public class J extends F {
+
+    private String i = "init";
+
+    private long e = 1234;
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public void ab() {
+        System.out.println();
+    }
+
+    public double ee() {
+        return 100.500;
+    }
+
+    public double ad() {
+        return 11;
+    }
+
+    public int af() {
+        return -1;
+    }
+
+    public int cc() {
+        return 42;
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public byte oo() {
+        return 2;
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+}
