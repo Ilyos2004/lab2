5e898e39a67aacd1ca04f7e0efd76a2cf2a2d03b
red <red@mail.com>
2024-05-24 00:32:37 +0300

    r8

---
+++ commit8/F.java
@@ -0,0 +1,26 @@
+public class F implements D {
+
+    private byte d = 1;
+
+    private String h = "test";
+
+    public Object gg() {
+        return new java.util.Random();
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-6);
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+}
