34201418f1f71ce3655211b3e945cf724189048d
blue <blue@mail.com>
2024-05-24 00:32:24 +0300

    r1

---
+++ commit1/J.java
@@ -0,0 +1,18 @@
+public class J extends F {
+
+    private String i = "init";
+
+    private long e = 1234;
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public void ab() {
+        System.out.println();
+    }
+}
